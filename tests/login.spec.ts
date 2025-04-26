f2d00e097bae4a402018d49b9d6bb3e63ebb51f7
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-04-26 15:25:52 +0200

    Teraz Ty – adnotacje i tagi

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -37,7 +37,7 @@ test.describe('User login to Demobank', () => {
 
   test(
     'unsuccessful login too short username',
-    { tag: '@login' },
+    { tag: ['@login', '@unhappy_path'] },
     async ({ page }) => {
       // Arrange
       const incorrectUserId = 'tester';
@@ -54,7 +54,7 @@ test.describe('User login to Demobank', () => {
 
   test(
     'unsuccessful login too short password',
-    { tag: '@login' },
+    { tag: ['@login', '@unhappy_path'] },
     async ({ page }) => {
       // Arrange
       const userId = loginData.userId;
