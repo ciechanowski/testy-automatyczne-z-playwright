b0cc295d19784d02b419e9ff1849e28f9b3a63c9
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-05-01 22:58:24 +0200

    Tagi i powiązanie z wymaganiami

--- tests/webpage.spec.ts
+++ tests/webpage.spec.ts
@@ -3,7 +3,7 @@ import { CommentsPage } from '../src/pages/comments.page';
 import { HomePage } from '../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
-test('home page title', async ({ page }) => {
+test('home page title', { tag: '@GAD-R01-01' }, async ({ page }) => {
   // Arrange
   const homePage = new HomePage(page);
 
@@ -15,7 +15,7 @@ test('home page title', async ({ page }) => {
   expect(title).toContain('GAD');
 });
 
-test('articles page title', async ({ page }) => {
+test('articles page title', { tag: '@GAD-R01-02' }, async ({ page }) => {
   // Arrange
   const articlePage = new ArticlesPage(page);
 
@@ -27,7 +27,7 @@ test('articles page title', async ({ page }) => {
   expect(title).toContain('Articles');
 });
 
-test('comments page title', async ({ page }) => {
+test('comments page title', { tag: '@GAD-R01-02' }, async ({ page }) => {
   // Arrange
   const commentsPage = new CommentsPage(page);
 
