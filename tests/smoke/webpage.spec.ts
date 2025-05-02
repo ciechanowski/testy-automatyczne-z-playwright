dff61d426e7f34944e917bde48ad2f599a52fb10
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-05-02 17:19:06 +0200

    Komponent Main Menu

---
+++ tests/smoke/webpage.spec.ts
@@ -0,0 +1,42 @@
+import { ArticlesPage } from '../../src/pages/articles.page';
+import { CommentsPage } from '../../src/pages/comments.page';
+import { HomePage } from '../../src/pages/home.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify service main pages', () => {
+  test('home page title', { tag: '@GAD-R01-01' }, async ({ page }) => {
+    // Arrange
+    const homePage = new HomePage(page);
+
+    // Act
+    await homePage.goto();
+
+    // Assert
+    const title = await homePage.title();
+    expect(title).toContain('GAD');
+  });
+
+  test('articles page title', { tag: '@GAD-R01-02' }, async ({ page }) => {
+    // Arrange
+    const articlePage = new ArticlesPage(page);
+
+    // Act
+    await articlePage.goto();
+
+    // Assert
+    const title = await articlePage.title();
+    expect(title).toContain('Articles');
+  });
+
+  test('comments page title', { tag: '@GAD-R01-02' }, async ({ page }) => {
+    // Arrange
+    const commentsPage = new CommentsPage(page);
+
+    // Act
+    await commentsPage.goto();
+
+    // Assert
+    const title = await commentsPage.title();
+    expect(title).toContain('Comments');
+  });
+});
