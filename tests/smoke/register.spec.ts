25892319b565952bf144134afcc053179181e66d
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-05-04 16:46:34 +0200

    Refaktoryzacja użycia modelu i testy negatywne

--- tests/smoke/register.spec.ts
+++ tests/smoke/register.spec.ts
@@ -39,13 +39,62 @@ test.describe('Verify register', () => {
       expect.soft(titleLogin).toContain('Login');
 
       // Assert
-      await loginPage.login(
-        registerUserData.userEmail,
-        registerUserData.userPassword,
-      );
+      await loginPage.login({
+        userEmail: registerUserData.userEmail,
+        userPassword: registerUserData.userPassword,
+      });
+
       const welcomePage = new WelcomePage(page);
       const titleWelcome = await welcomePage.title();
       expect(titleWelcome).toContain('Welcome');
+    },
+  );
+
+  test(
+    'not register with incorrect data - invalid email',
+    { tag: '@GAD-R03-04' },
+    async ({ page }) => {
+      // Arrange
+      const registerUserData: RegisterUser = {
+        userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+        userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+        userEmail: 'test.email.com',
+        userPassword: faker.internet.password(),
+      };
+
+      const expectedErrorText = 'Please provide a valid email address';
+      const registerPage = new RegisterPage(page);
+
+      // Act
+      await registerPage.goto();
+      await registerPage.register(registerUserData);
+
+      // Assert
+      await expect(registerPage.emailErrorText).toHaveText(expectedErrorText);
+    },
+  );
+
+  test(
+    'not register with incorrect data - email not provided',
+    { tag: '@GAD-R03-04' },
+    async ({ page }) => {
+      // Arrange
+      const expectedErrorText = 'This field is required';
+      const registerPage = new RegisterPage(page);
+
+      // Act
+      await registerPage.goto();
+      await registerPage.userFirstNameInput.fill(
+        faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+      );
+      await registerPage.userLastNameInput.fill(
+        faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+      );
+      await registerPage.userPasswordInput.fill(faker.internet.password());
+      await registerPage.registerButton.click();
+
+      // Assert
+      await expect(registerPage.emailErrorText).toHaveText(expectedErrorText);
     },
   );
 });
