614f55bba4eeecf47f32de7860340cb9fd50674c
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-10-31 23:50:58 +0100

    Exercises function overloading

--- exercises/9-ts-features/4-1-function-overloading.ts
+++ exercises/9-ts-features/4-1-function-overloading.ts
@@ -29,3 +29,18 @@ export {} // Ignore this line
 // -------------------------
 
 // 👇 Your code here 👇 
+function combine(a: string, b: string): string 
+function combine(a: number, b: number): number 
+function combine(a: any, b: any): any {
+    if (typeof a === "string" && typeof b === "string") {
+        return a + b
+    }
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b
+    }
+}
+
+const combinedStrings = combine("Hello", "World") 
+console.log("Combined strings: " + combinedStrings) 
+const combinedNumbers = combine(7, 8) 
+console.log("Combined numbers: " + combinedNumbers) 
