7538e1d7747c9b8e67bbb31d31eb8fa861df1a6a
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-11-11 00:08:36 +0100

    Rozszerzenie testu e2e - usunięcie zasobu

--- src/pages/article.page.ts
+++ src/pages/article.page.ts
@@ -7,11 +7,20 @@ export class ArticlePage extends BasePage {
   mainMenu: MainMenuComponent;
   articleTitle: Locator;
   articleBody: Locator;
+  deleteIcon: Locator;
 
   constructor(page: Page) {
     super(page);
     this.mainMenu = new MainMenuComponent(this.page);
     this.articleTitle = this.page.getByTestId('article-title');
     this.articleBody = this.page.getByTestId('article-body');
+    this.deleteIcon = this.page.getByTestId('delete');
+  }
+
+  async deleteArticle(): Promise<void> {
+    this.page.on('dialog', async (dialog) => {
+      await dialog.accept();
+    });
+    this.deleteIcon.click();
   }
 }
