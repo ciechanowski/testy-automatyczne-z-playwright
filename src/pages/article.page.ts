ddb443b9c918e54657ab4261b33977c3dda3a956
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-11-20 20:04:24 +0100

    Nowy typ testu e2e. Wymagania, analiza i pierwsze kroki

--- src/pages/article.page.ts
+++ src/pages/article.page.ts
@@ -8,6 +8,8 @@ export class ArticlePage extends BasePage {
   articleTitle: Locator;
   articleBody: Locator;
   deleteIcon: Locator;
+  addCommentButton: Locator;
+  alertPopup: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -15,6 +17,8 @@ export class ArticlePage extends BasePage {
     this.articleTitle = this.page.getByTestId('article-title');
     this.articleBody = this.page.getByTestId('article-body');
     this.deleteIcon = this.page.getByTestId('delete');
+    this.addCommentButton = this.page.locator('#add-new');
+    this.alertPopup = this.page.getByTestId('alert-popup');
   }
 
   async deleteArticle(): Promise<void> {
