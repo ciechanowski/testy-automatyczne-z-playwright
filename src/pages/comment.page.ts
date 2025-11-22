54c3e9efbe54c7f7ec319183374f271ae9d935af
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-11-23 00:48:42 +0100

    Modyfikacja zasobu i weryfikacja edycji

--- src/pages/comment.page.ts
+++ src/pages/comment.page.ts
@@ -6,10 +6,16 @@ export class CommentPage extends BasePage {
   url = '/comment.html';
   mainMenu: MainMenuComponent;
   commentBody: Locator;
+  editButton: Locator;
+  alertPopup: Locator;
+  returnLink: Locator;
 
   constructor(page: Page) {
     super(page);
     this.mainMenu = new MainMenuComponent(this.page);
     this.commentBody = this.page.getByTestId('comment-body');
+    this.editButton = this.page.getByTestId('edit');
+    this.alertPopup = this.page.getByTestId('alert-popup');
+    this.returnLink = this.page.getByTestId('return');
   }
 }
