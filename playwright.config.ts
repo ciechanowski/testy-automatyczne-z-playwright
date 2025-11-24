f05af3e62b6820b1e3419647e44333a55b8027eb
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-11-24 16:17:45 +0100

    Logowanie jako projekt

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,12 +27,13 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'iphone',
-      use: { ...devices['iPhone 12 Mini'] },
+      name: 'setup',
+      testMatch: '**.setup.ts',
     },
     {
-      name: 'smoke',
-      testDir: './tests/smoke',
+      name: 'logged',
+      grep: /@logged/,
+      dependencies: ['setup'],
     },
   ],
 });
---
+++ tests/setup/login.setup.ts
@@ -0,0 +1,20 @@
+import { LoginPage } from '../../src/pages/login.page';
+import { WelcomePage } from '../../src/pages/welcome.page';
+import { testUser1 } from '../../src/test-data/user-data';
+import { expect, test as setup } from '@playwright/test';
+
+setup('login with correct credentials', async ({ page }) => {
+  // Arrange
+  const expectedWelcomeTitle = 'Welcome';
+  const loginPage = new LoginPage(page);
+  const welcomePage = new WelcomePage(page);
+
+  // Act
+  await loginPage.goto();
+  await loginPage.login(testUser1);
+
+  const titleWelcome = await welcomePage.getTitle();
+
+  // Assert
+  expect(titleWelcome).toContain(expectedWelcomeTitle);
+});
