81e239b160a190325819f5fdee6ee6447f1f6cec
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-11-25 18:59:51 +0100

    Teraz Ty - Refaktoryzacja testów do sesji

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'chromium',
+      name: 'chromium-non-logged',
       grepInvert: /@logged/,
       use: { ...devices['Desktop Chrome'] },
     },
@@ -36,10 +36,11 @@ export default defineConfig({
       testMatch: '**.setup.ts',
     },
     {
-      name: 'logged',
+      name: 'chromium-logged',
       grep: /@logged/,
       dependencies: ['setup'],
       use: {
+        ...devices['Desktop Chrome'],
         storageState: STORAGE_STATE,
       },
     },
--- tests/end-to-end/article-lifecycle.spec.ts
+++ tests/end-to-end/article-lifecycle.spec.ts
@@ -2,31 +2,25 @@ import { prepareRandomArticle } from '../../src/factories/article.factory';
 import { AddArticleModel } from '../../src/models/article.model';
 import { ArticlePage } from '../../src/pages/article.page';
 import { ArticlesPage } from '../../src/pages/articles.page';
-import { LoginPage } from '../../src/pages/login.page';
-import { testUser1 } from '../../src/test-data/user-data';
 import { AddArticleView } from '../../src/views/add-article.view';
 import { expect, test } from '@playwright/test';
 
 test.describe.configure({ mode: 'serial' });
 test.describe('Create, verify and delete article', () => {
-  let loginPage: LoginPage;
   let articlesPage: ArticlesPage;
   let addArticleView: AddArticleView;
   let articleData: AddArticleModel;
   let articlePage: ArticlePage;
 
   test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
     articlesPage = new ArticlesPage(page);
     addArticleView = new AddArticleView(page);
     articlePage = new ArticlePage(page);
 
-    await loginPage.goto();
-    await loginPage.login(testUser1);
     await articlesPage.goto();
   });
 
-  test('create new article', { tag: '@GAD-R04-01' }, async () => {
+  test('create new article', { tag: ['@GAD-R04-01', '@logged'] }, async () => {
     // Arrange
     articleData = prepareRandomArticle();
 
@@ -42,32 +36,40 @@ test.describe('Create, verify and delete article', () => {
       .toHaveText(articleData.body, { useInnerText: true });
   });
 
-  test('user can access single article', { tag: '@GAD-R04-03' }, async () => {
-    // Act
-    await articlesPage.gotoArticle(articleData.title);
+  test(
+    'user can access single article',
+    { tag: ['@GAD-R04-03', '@logged'] },
+    async () => {
+      // Act
+      await articlesPage.gotoArticle(articleData.title);
 
-    // Assert
-    await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
-    await expect
-      .soft(articlePage.articleBody)
-      .toHaveText(articleData.body, { useInnerText: true });
-  });
+      // Assert
+      await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
+      await expect
+        .soft(articlePage.articleBody)
+        .toHaveText(articleData.body, { useInnerText: true });
+    },
+  );
 
-  test('user can delete his own article', { tag: '@GAD-R04-04' }, async () => {
-    // Arrange
-    const expectedArticlesTitle = 'Articles';
-    const expectNoResultText = 'No data';
-    await articlesPage.gotoArticle(articleData.title);
+  test(
+    'user can delete his own article',
+    { tag: ['@GAD-R04-04', '@logged'] },
+    async () => {
+      // Arrange
+      const expectedArticlesTitle = 'Articles';
+      const expectNoResultText = 'No data';
+      await articlesPage.gotoArticle(articleData.title);
 
-    // Act
-    await articlePage.deleteArticle();
+      // Act
+      await articlePage.deleteArticle();
 
-    // Assert
-    await articlesPage.waitForPageToLoadUrl();
-    const title = await articlesPage.getTitle();
-    expect(title).toContain(expectedArticlesTitle);
+      // Assert
+      await articlesPage.waitForPageToLoadUrl();
+      const title = await articlesPage.getTitle();
+      expect(title).toContain(expectedArticlesTitle);
 
-    await articlesPage.searchArticle(articleData.title);
-    await expect(articlesPage.noResultText).toHaveText(expectNoResultText);
-  });
+      await articlesPage.searchArticle(articleData.title);
+      await expect(articlesPage.noResultText).toHaveText(expectNoResultText);
+    },
+  );
 });
