a934ffc8706ce9c2bc31892ed0ac8a5c4bf5c923
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-12-04 22:33:39 +0100

    Rozszerzone oczekiwanie odpowiedzi z API

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,3 +1,4 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 import { waitForResponse } from '@_src/utils/wait.util';
@@ -95,4 +96,35 @@ test.describe('Verify articles', () => {
       },
     );
   });
+
+  test(
+    'should return created article from API',
+    { tag: ['@GAD-R07-04', '@logged'] },
+    async ({ addArticleView, page }) => {
+      // Arrange
+      const articleData = prepareRandomArticle();
+      const responsePromise = page.waitForResponse(
+        (response) => {
+          // console.log(
+          //   response.request().method(),
+          //   response.url(),
+          //   response.status(),
+          // );
+          return (
+            response.url().includes('api/articles') &&
+            response.request().method() == 'GET'
+          );
+        },
+        { timeout: RESPONSE_TIMEOUT },
+      );
+
+      // Act
+      const articlePage = await addArticleView.createArticle(articleData);
+      const response = await responsePromise;
+
+      // Assert
+      await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
+      expect(response.ok()).toBeTruthy();
+    },
+  );
 });
