5e1eafea3411eb397f270c236ea23ba0ffaaf767
Michał Ciechanowski <mciechanowski1991@gmail.com>
2026-02-07 18:18:21 +0100

    Logowanie się w API i tworzenie zasobu

--- tests/api/articles.integration.spec.ts
+++ tests/api/articles.integration.spec.ts
@@ -1,29 +1,83 @@
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
+import { testUser1 } from '@_src/test-data/user-data';
 
-test.describe('Verify articles CRUD operations @api', () => {
-  test('should not create an article without a logged-in user', async ({
-    request,
-  }) => {
-    // Arrange
-    const expectedStatusCode = 401;
-    const articlesUrl = '/api/articles';
+test.describe(
+  'Verify articles CRUD operations @api',
+  { tag: ['@GAD-R08-03', '@api'] },
+  () => {
+    test('should not create an article without a logged-in user', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedStatusCode = 401;
+      const articlesUrl = '/api/articles';
 
-    const randomArticleData = prepareRandomArticle();
+      const randomArticleData = prepareRandomArticle();
 
-    const articleData = {
-      title: randomArticleData.title,
-      body: randomArticleData.body,
-      date: '2026-02-07T13:35:09.614Z',
-      image: '',
-    };
+      const articleData = {
+        title: randomArticleData.title,
+        body: randomArticleData.body,
+        date: '2026-02-07T13:35:09.614Z',
+        image: '',
+      };
 
-    // Act
-    const response = await request.post(articlesUrl, {
-      data: articleData,
+      // Act
+      const response = await request.post(articlesUrl, {
+        data: articleData,
+      });
+
+      // Assert
+      expect(response.status()).toBe(expectedStatusCode);
     });
 
-    // Assert
-    expect(response.status()).toBe(expectedStatusCode);
-  });
-});
+    test('should not create an article with a logged-in user', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedStatusCode = 201;
+
+      // Login
+      const loginUrl = '/api/login';
+      const userData = {
+        email: testUser1.userEmail,
+        password: testUser1.userPassword,
+      };
+      const responseLogin = await request.post(loginUrl, {
+        data: userData,
+      });
+      const responseLoginJson = await responseLogin.json();
+
+      // Act
+      const articlesUrl = '/api/articles';
+
+      const randomArticleData = prepareRandomArticle();
+      const articleData = {
+        title: randomArticleData.title,
+        body: randomArticleData.body,
+        date: '2026-02-07T13:35:09.614Z',
+        image:
+          '.\\data\\images\\256\\tester-app_9f26eff6-2390-4460-8829-81a9cbe21751.jpg',
+      };
+
+      const headers = {
+        Authorization: `Bearer ${responseLoginJson.access_token}`,
+      };
+      const responseArticle = await request.post(articlesUrl, {
+        headers,
+        data: articleData,
+      });
+
+      // Assert
+      const actualResponseStatus = responseArticle.status();
+      expect(
+        actualResponseStatus,
+        `status code expected ${expectedStatusCode}, but received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode);
+
+      const article = await responseArticle.json();
+      expect.soft(article.title).toEqual(articleData.title);
+      expect.soft(article.body).toEqual(articleData.body);
+    });
+  },
+);
