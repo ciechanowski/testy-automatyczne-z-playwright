14b3b72778d85bd170b58267a2fbf57340405495
Michał Ciechanowski <mciechanowski1991@gmail.com>
2026-02-28 17:29:58 +0100

    Zabezpieczenie testów za pomocą API

--- config/global.setup.ts
+++ config/global.setup.ts
@@ -1,13 +1,24 @@
 import { STORAGE_STATE } from '@_pw-config';
+import { FullConfig, request } from '@playwright/test';
 import * as fs from 'fs';
 
-async function globalSetup(): Promise<void> {
+async function globalSetup(config: FullConfig): Promise<void> {
   if (fs.existsSync(STORAGE_STATE)) {
     fs.unlinkSync(STORAGE_STATE);
   }
-
-  // console.log('⚠️  URL:', process.env.BASE_URL);
   // console.log('⚠️  Global setup');
+  const baseURL = config.projects[0].use.baseURL;
+  const requestContext = await request.newContext();
+
+  try {
+    await requestContext.get(baseURL!);
+    // const response = await requestContext.get(baseURL!);
+    // console.log('Response ok?', response.ok());
+  } catch {
+    throw new Error(
+      `❌ Failed to connect to ${baseURL} check if the application is running and the baseUrl is correct`,
+    );
+  }
 }
 
 export default globalSetup;
